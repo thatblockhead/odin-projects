84f79a80bfe7e912559cbb244417090f2a645dfc
thatblockhead <52268541+thatblockhead@users.noreply.github.com>
2022-11-16 14:34:00 -0800

    First working version

--- Etch-a-Sketch/scripts.js
+++ Etch-a-Sketch/scripts.js
@@ -1,9 +1,13 @@
 
 const gridContainer = document.querySelector('.container')
 
-for (let i = 0; i < 256; i++) {
+function buildGrid(x) {
+    for (let i = 0; i < x**2; i++) {
     gridContainer.innerHTML += ('<div class="item"></div>')
+    }
 }
+
+buildGrid(16)
 
 gridContainer.addEventListener("mouseover", function(event) {
     if (event.target.classList.contains("item")) {
@@ -13,9 +17,14 @@ gridContainer.addEventListener("mouseover", function(event) {
 
 function servePrompt() {
     let gridNum = prompt("Enter a value between 2 and 100 to specify the dimensions of the grid")
-    if (gridNum > 100 || gridNum < 2) {
-        servePrompt()
+    if (isNaN(Number(gridNum)) || Number(gridNum) > 100 || Number(gridNum) < 2) {
+        alert('Enter a number between 2 and 100')
+        return 0
     }
-    gridContainer.style.gridTemplateColumns = "null"
+    let percent = (Number(gridNum) ** 2) / 100
+    console.log(percent)
+    gridContainer.innerHTML = ''
+    buildGrid(gridNum)
+    gridContainer.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`
     gridContainer.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`
 }
