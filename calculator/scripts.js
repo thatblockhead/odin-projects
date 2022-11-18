2674dac84ee7924c61b25500ffe101566ac5798a
thatblockhead <52268541+thatblockhead@users.noreply.github.com>
2022-11-17 17:49:27 -0800

    Checkpoint before messing with div structure

---
+++ calculator/scripts.js
@@ -0,0 +1,29 @@
+function add(x,  y) {
+    return x + y
+}
+
+function subtract(x, y) {
+    return x - y
+}
+
+function multiply(x, y) {
+    return x * y
+}
+
+function divide(x, y) {
+    // if y === 0 SNARKY
+    return x / y
+}
+
+function operate(operator, x, y) {
+    switch(operator) {
+        case "+":
+            return add(x, y)
+        case "-":
+            return subtract(x, y)
+        case "*":
+            return multiply(x, y)
+        case "/":
+            return divide(x, y)
+    }
+}
